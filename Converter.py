6d5921a9bd5f174e28989a666355affcd5e408b8
R. T <RTShields@users.noreply.github.com>
2021-11-09 17:42:08 -0600

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,66 @@
+#################################
+### Convert Python to EXE ###
+#################################
+### This program is designed to do restore backup froma zip
+
+import os
+from os.path import exists
+from os import listdir
+from os.path import isfile, join
+import shutil
+import glob
+import subprocess
+import sys
+
+
+py_file = glob.glob('*.py')
+icon_file = glob.glob('*.ico')
+
+for files in py_file:
+	if files == 'Converter.py':pass
+	else:file = files
+
+pypath = sys.path
+path = pypath[5]
+Core_path = path[:-13]+'scripts\\'
+Project_path = os.path.dirname(os.path.abspath(__file__))+'\\'
+
+def Move_PyInstaller(Core,Project):
+	script = 'pyinstaller.exe'
+	rFrom = Core+script
+	rTo = Project+script
+	try:
+		shutil.copy(rFrom,rTo)
+	except:
+		print('Error moving PyInstaller.exe')
+
+def Convert(file):
+	global icon_file
+	global Project_path
+	with open('convert.bat','w') as con:
+		if len(icon_file) == 0:
+			con.write('pyinstaller -w -F -i '+file)
+		else:
+			icon = icon_file[0]
+			con.write('pyinstaller -w -F -i "'+icon+'" "'+file+'"')
+		con.close
+	subprocess.call('convert.bat')
+	exfile = file[:-3]+'.exe'
+	Dist_path = Project_path+'dist\\'+exfile
+	try:
+		shutil.move(Dist_path,Project_path+exfile)
+	except:
+		print("Can't find executable.")
+
+def cleanup():
+	garbage = [	'__pycache__','build','dist']
+	bags = [file[:-3]+'.spec','pyinstaller.exe','convert.bat']
+	try:
+		for bag in garbage:shutil.rmtree(bag)
+		for trash in bags:os.remove(trash)
+	except:
+		print('All Clean!')
+
+Move_PyInstaller(Core_path,Project_path)
+Convert(file)
+cleanup()
---
+++ New Projects.py
@@ -0,0 +1,103 @@
+# New Project Generator
+# Creates a folder, initial script file, and codestyle checker
+# for said script, then the pyinstaller converter
+
+import os
+import shutil
+import glob
+import sys
+import re
+
+
+# find Python folder
+dist = sys.executable
+redist = dist.replace(' ', '_')
+xout = len(dist) - 10
+pydist = re.escape(redist[:xout])
+f_pydist = pydist.replace('_', ' ')
+print(f_pydist)
+scripts = ['pyinstaller.exe', 'pycodestyle.exe', 'pyflakes.exe', 'Converter.py']
+
+make_mods = 0
+for item in scripts:
+    file_check = f_pydist + 'Scripts\\\\' + item
+    if os.path.isfile(file_check) is False:
+        make_mods += 1
+    else:
+        make_mods += 0
+
+
+def ModInstaller():
+    with open('Modules Installer.bat', 'w') as Mods:
+        if os.path.isfile(pydist + 'Scirpts\\pyinstaller.exe') is False:
+            Mods.write('pip install pyinstaller\n')
+        if os.path.isfile(pydist + 'Scirpts\\pycodestyle.exe') is False:
+            Mods.write('pip install pycodestyle\n')
+        if os.path.isfile(pydist + 'Scirpts\\pyflakes.exe') is False:
+            Mods.write('pip install pyflakes\n')
+        if os.path.isfile(pydist + 'Scirpts\\Converter.py') is False:
+            Mods.write('Please copy/paste Converter.py manually.')
+        Mods.close()
+    return
+
+
+if make_mods >= 1:
+    ModInstaller()
+
+
+def Create_stuff(file, Fpath, desc):
+    # Create folder and file for the project
+    pyfile = Fpath + '\\' + file + '.py'
+
+    if os.path.isdir(file) is True:
+        # Create the work folder
+        folder2 = input('Project folder already exist, try another.\n')
+        os.mkdir(folder2)
+    else:
+        os.mkdir(file)
+
+    with open(pyfile, 'w') as creator:
+        creator.write('# ' + file + '\n')
+        creator.write('# Created by John Palmer\n')
+        Tchar = len(desc)
+        print(Tchar)
+        if Tchar <= 60:
+            creator.write('# ' + desc)
+            creator.close()
+        else:
+            Tlines = 1 + int(Tchar / 60)
+            lcount = 0
+            for line in range(Tlines):
+                pout = line * 60
+                print(pout)
+
+                creator.write('# ' + desc[pout:pout + 60] + '\n')
+        creator.close()
+    return
+
+
+def Crafter_checkers(file, Fpath, PyPath):
+    # Move CodeStyle and PyFlakes to folder, creater checker script
+    global scripts
+    for item in scripts:
+        shutil.copy(PyPath + item, Fpath + item)
+
+    with open(file + '\\checker.bat', 'w') as king:
+        king.write('pycodestyle --ignore=E501 "' + file + '.py">Revisions.txt\n')
+        king.write('pyflakes "' + file + '.py >> Revisions.txt')
+
+
+def Main():
+    work_file = input('What is the name of the porject?')
+    desc = input('Describe what this script does:\n')
+    # work_file = 'Python Breaker'
+    # desc = '0123456789012345678901234567890123456798ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    folder_path = 'D:\\Documents\\Documents\\[-] Coding Projects\\-01- Work Scripts'
+    dest_path = folder_path + '\\' + work_file
+    py_path = pydist + 'Scripts\\'
+    folder = folder_path + '\\' + work_file
+    Create_stuff(work_file, dest_path, desc)
+    Crafter_checkers(work_file, dest_path + '\\', py_path)
+
+
+Main()
